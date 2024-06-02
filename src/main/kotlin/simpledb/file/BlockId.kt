ad56cae22dcab199bbab42a4767137ba9001c1b0
sobatha <natsuki.yokoizo.ny@gmail.com>
2024-06-02 15:23:55 +0900

    implement Page

--- src/main/kotlin/simpledb/file/BlockId.kt
+++ src/main/kotlin/simpledb/file/BlockId.kt
@@ -3,11 +3,4 @@ package org.example.simpledb.file
 /*
  * identifies a specific block by irs file name and logical block number
  */
-class BlockId(private val fileName: String, private val number: Int) {
-    override fun toString(): String {
-        return "[file $fileName, block $number]"
-    }
-    override fun hashCode(): Int {
-        return toString().hashCode()
-    }
-}
+data class BlockId(private val fileName: String, private val number: Int)
---
+++ src/main/kotlin/simpledb/file/Page.kt
@@ -0,0 +1,62 @@
+package org.example.simpledb.file
+
+import java.nio.ByteBuffer
+import java.nio.charset.Charset
+
+/*
+ * holds the contents of a disk block
+ * constructor creates a page that gets its memory from an operating system IO buffer.
+ * second constructor creates a page that gets its memory from a Java array.
+ */
+class Page {
+    private val DEFAULT_CHARSET: Charset = Charsets.US_ASCII
+    private var byteBuffer: ByteBuffer
+
+    constructor(blockSize: Int) {
+        byteBuffer = ByteBuffer.allocateDirect(blockSize)
+    }
+
+    constructor(bytes: ByteArray) : this(0) {
+        byteBuffer = ByteBuffer.wrap(bytes)
+    }
+
+    fun getInt(offset: Int) = byteBuffer.getInt(offset)
+
+    fun setInt(offset: Int, n: Int) = byteBuffer.putInt(offset, n)
+
+    fun getBytes(offset: Int): ByteArray {
+        byteBuffer.position(offset)
+        val length = byteBuffer.getInt()
+        val b = ByteArray(length)
+        byteBuffer.get(b)
+        return b
+    }
+
+    fun setBytes(offset: Int, b: ByteArray) {
+        byteBuffer.position(offset)
+        byteBuffer.putInt(b.size)
+        byteBuffer.put(b)
+    }
+
+    fun getString(offset: Int): String {
+        val b = getBytes(offset)
+        return String(b, DEFAULT_CHARSET)
+    }
+
+    fun setString(offset: Int, string: String) {
+        val b = string.toByteArray(DEFAULT_CHARSET)
+        setBytes(offset, b)
+    }
+
+    fun contents(): ByteBuffer {
+        byteBuffer.position(0)
+        return byteBuffer
+    }
+
+    companion object {
+        private val CHARSET: Charset = Charsets.US_ASCII
+        fun maxLength(strLen: Int) {
+            val bytePerChar = CHARSET.newEncoder().maxBytesPerChar()
+        }
+    }
+}
