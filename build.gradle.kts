a34d0d3b72cabe622dbfad1cc5a1b2611b0d7713
sobatha <natsuki.yokoizo.ny@gmail.com>
2024-06-02 17:09:01 +0900

    add test library

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,8 @@ repositories {
 
 dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test")
+    testImplementation("io.kotest:kotest-runner-junit5:5.0.0")
+    testImplementation("io.kotest:kotest-assertions-core:5.0.0")
 }
 
 tasks.test {
---
+++ src/test/kotlin/simpledb/file/FileMgrTest.kt
@@ -0,0 +1,8 @@
+package simpledb.file
+
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+
+class FileMgrTest : FunSpec({
+
+})
