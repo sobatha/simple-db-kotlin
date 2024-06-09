16d25c0d1a2462042181d28032382aaf8ea88753
sobatha <natsuki.yokoizo.ny@gmail.com>
2024-06-10 01:34:20 +0900

    add buffer manager implementations

---
+++ src/test/kotlin/simpledb/buffer/Buffer.kt
@@ -0,0 +1,43 @@
+package simpledb.buffer
+
+import org.example.simpledb.file.BlockId
+import org.example.simpledb.file.FileMgr
+import org.example.simpledb.file.Page
+import simpledb.log.LogMgr
+
+class Buffer(private val fileMgr: FileMgr, private val logMgr: LogMgr) {
+    val contents = Page(fileMgr.blockSize)
+    var block: BlockId? = null
+        private set
+    private var pins = 0
+    var modificationCount = 0
+        private set
+    private var lsn = 0
+    val isPinned: Boolean
+        get() = pins > 0
+
+    fun setModified(modifiedNumber: Int, lsn: Int) {
+        modificationCount = modifiedNumber
+        if (lsn >= 0) this.lsn = lsn
+    }
+
+    fun assignToBlock(b: BlockId) {
+        flush()
+        block = b
+        fileMgr.read(b, contents)
+        pins = 0
+    }
+
+    fun flush() {
+        if (modificationCount >= 0 && block != null) {
+            logMgr.flush(lsn)
+            fileMgr.read(block!!, contents)
+            pins = 0
+        }
+    }
+
+    fun pin() = pins++
+
+    fun unpin() = pins--
+
+}
---
+++ src/test/kotlin/simpledb/buffer/BufferMgr.kt
@@ -0,0 +1,62 @@
+package simpledb.buffer
+
+import org.example.simpledb.file.BlockId
+import org.example.simpledb.file.FileMgr
+import simpledb.log.LogMgr
+
+class BufferMgr(val fileMgr: FileMgr, val logMgr: LogMgr, var numBuff: Int) {
+    private val bufferPool: MutableList<Buffer> = mutableListOf()
+    @Volatile
+    var numAvailable = 0
+        private set
+
+    private val MAX_TIME = 10000 // 10seconds for wait limits
+    private val lock = java.lang.Object()
+
+    fun waitingTooLong(startTime: Long) = System.currentTimeMillis() - startTime > MAX_TIME
+
+    @Synchronized
+    fun flushAll(modifiedCount: Int) {
+        bufferPool
+            .filter { it.modificationCount == modifiedCount }
+            .map { it.flush() }
+    }
+
+    @Synchronized
+    fun unpin(buffer: Buffer) {
+        buffer.unpin()
+        if (!buffer.isPinned) {
+            numAvailable++
+            lock.notifyAll()
+        }
+    }
+    @Synchronized
+    fun pin(blk: BlockId): Buffer {
+        val timeStamp = System.currentTimeMillis()
+        var buffer = tryToPin(blk)
+        while (buffer == null && !waitingTooLong(timeStamp)) {
+            lock.wait(MAX_TIME.toLong())
+            buffer = tryToPin(blk)
+        }
+        return buffer ?: throw InterruptedException()
+    }
+
+    @Synchronized
+    fun tryToPin(blk: BlockId):Buffer? {
+        val buffer = findExistingBuffer(blk) ?: run {
+            chooseUnpinnedBuffer() ?: return null
+        }
+        return buffer.apply {
+            if(!isPinned) numAvailable--
+            pin()
+        }
+    }
+
+    private fun chooseUnpinnedBuffer(): Buffer? {
+        return bufferPool.find { !it.isPinned }
+    }
+
+    private fun findExistingBuffer(block: BlockId): Buffer? {
+        return bufferPool.find { it.block == block && it.block != null }
+    }
+}
