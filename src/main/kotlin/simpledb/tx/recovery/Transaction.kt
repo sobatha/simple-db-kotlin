feb9a5916f96ab5fee982e5b578d01c1fd7e7d6f
sobatha <natsuki.yokoizo.ny@gmail.com>
2024-07-14 15:22:49 +0900

    implement concurrency

--- src/main/kotlin/simpledb/tx/recovery/Transaction.kt
+++ src/main/kotlin/simpledb/tx/recovery/Transaction.kt
@@ -1,5 +1,6 @@
 package simpledb.tx.recovery
 
+import org.example.simpledb.tx.concurrency.ConcurrencyMgr
 import simpledb.buffer.Buffer
 import simpledb.file.BlockId
 import simpledb.file.FileMgr
@@ -9,10 +10,12 @@ import simpledb.log.LogMgr
 
 class Transaction(val fileMgr: FileMgr, val logMgr: LogMgr, val bufferMgr: BufferMgr) {
     companion object {
+        private val END_OF_FILE = -1
         private var nextTxNumber = 0
     }
     private val txNum = getNextTxNumber()
-    val recoveryMgr = RecoveryMgr(this, txNum, logMgr, bufferMgr)
+    private val recoveryMgr = RecoveryMgr(this, txNum, logMgr, bufferMgr)
+    private val concurrencyMgr = ConcurrencyMgr()
     private val myBuffers = mutableMapOf<BlockId, Buffer>()
 
     @Synchronized
@@ -33,6 +36,7 @@ class Transaction(val fileMgr: FileMgr, val logMgr: LogMgr, val bufferMgr: Buffe
     }
 
     fun setString(blk: BlockId, offset: Int, log_value: String, is_need_logged: Boolean) {
+        concurrencyMgr.xLock(blk)
         val buff = bufferMgr.getBuffer(blk)!!
         val page = buff.contents
         var lsn = -1
@@ -41,38 +45,54 @@ class Transaction(val fileMgr: FileMgr, val logMgr: LogMgr, val bufferMgr: Buffe
         }
         page.setString(offset, log_value)
         buff.setModified(txNum, lsn)
-//        TODO("implement concurrency control")
     }
 
     fun setInt(blk: BlockId, offset: Int, log_value: Int, is_need_logged: Boolean) {
+        concurrencyMgr.xLock(blk)
         val buff = bufferMgr.getBuffer(blk)!!
-        val page = buff.contents
         var lsn = -1
         if (is_need_logged) {
             lsn = recoveryMgr.setInt(buff, offset, log_value)
         }
+        val page = buff.contents
         page.setInt(offset, log_value)
         buff.setModified(txNum, lsn)
-//        TODO("implement concurrency control")
     }
 
 
     fun commit() {
         recoveryMgr.commit()
+        concurrencyMgr.release()
         bufferMgr.unpinAll()
-//        TODO("implement concurrency control")
     }
 
     fun rollback() {
         recoveryMgr.rollback()
+        concurrencyMgr.release()
         bufferMgr.unpinAll()
-//        TODO("implement concurrency control")
+    }
+    fun getInt(blk: BlockId, i: Int): Int? {
+        concurrencyMgr.sLock(blk)
+        return bufferMgr.getBuffer(blk)?.contents?.getInt(i)
+    }
+    fun getString(blk: BlockId, i: Int): String? {
+        concurrencyMgr.sLock(blk)
+        return bufferMgr.getBuffer(blk)?.contents?.getString(i)
     }
 
-//        TODO("implement concurrency control")
-    fun getInt(blk: BlockId, i: Int): Int? = bufferMgr.getBuffer(blk)?.contents?.getInt(i)
-//        TODO("implement concurrency control")
-    fun getString(blk: BlockId, i: Int) = bufferMgr.getBuffer(blk)?.contents?.getString(i)
+    fun size(fileName: String): Int {
+        val dummyBlk = BlockId(fileName, END_OF_FILE)
+        concurrencyMgr.sLock(dummyBlk)
+        return fileMgr.length(fileName)
+    }
 
+    fun append(fileName: String): BlockId {
+        val dummyBlk = BlockId(fileName, END_OF_FILE)
+        concurrencyMgr.xLock(dummyBlk)
+        return fileMgr.append(fileName)
+    }
 
+    fun blkSize() = fileMgr.blockSize
+
+    fun availableBuffers() = bufferMgr.numAvailable
 }
